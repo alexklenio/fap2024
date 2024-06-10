221a8aa1c5924d47d1803cdd0140ca1d07d1c44a
alexklenio <alexlopes.see@gmail.com>
2024-06-10 19:16:28 -0300

    aulas iniciais

---
+++ Logica/soma.js
@@ -0,0 +1,12 @@
+// var a = 5;
+// var b = 2;
+// var soma = a + b;
+// console.log(soma);
+
+var a = 3;
+var b = 10;
+var c = 25;
+
+var soma = a + b + c;
+
+console.log("A soma dos três números é " + soma);
